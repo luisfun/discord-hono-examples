3482709a259915b3f7bc29326a90e7b17fae9973
Luis <101060519+LuisFun@users.noreply.github.com>
2025-05-11 05:31:44 +0900

    feat: add setCrossLog function to manage cross guild message logging

---
+++ workerd-2ch/src/db/set-cross-log.ts
@@ -0,0 +1,26 @@
+const maxLog = 100
+
+/**
+ * insert new message and delete old messages (max log 100)
+ */
+export const setCrossLog = (
+  db: D1Database,
+  cross_guild_id: string,
+  guild_id: string,
+  user_id: string,
+  message: string | undefined,
+) =>
+  db.batch([
+    db
+      .prepare(`INSERT INTO _${cross_guild_id} (guild_id, user_id, message, created_at) VALUES (?, ?, ?, ?)`)
+      .bind(guild_id, user_id, message ?? null, Date.now()),
+    db.prepare(`
+      DELETE FROM _${cross_guild_id}
+      WHERE id IN (
+        SELECT id
+        FROM _${cross_guild_id}
+        ORDER BY id ASC
+        LIMIT GREATEST((SELECT COUNT(*) FROM _${cross_guild_id}) - ${maxLog}, 0)
+      )
+    `),
+  ])
