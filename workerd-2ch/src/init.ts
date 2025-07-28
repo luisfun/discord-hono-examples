30c0f1ea8486405db09a16ceae088a1de149d318
Luis <101060519+LuisFun@users.noreply.github.com>
2025-07-28 17:15:21 +0900

    chore: Migrating from @cloudflare/workers-types to wrangler types

--- workerd-2ch/src/init.ts
+++ workerd-2ch/src/init.ts
@@ -1,9 +1,3 @@
 import { createFactory } from 'discord-hono'
 
-export type Env = {
-  Bindings: {
-    DB: D1Database
-  }
-}
-
-export const factory = createFactory<Env>()
+export const factory = createFactory<{ Bindings: Env }>()
