df5b7110f77866818851cceb2cea30d8e97a13e6
Luis <101060519+LuisFun@users.noreply.github.com>
2026-09-04 20:32:25 +0900

    fix: update response method in help component

--- workerd-subcommand-separation/src/handlers/get/help.ts
+++ workerd-subcommand-separation/src/handlers/get/help.ts
@@ -11,7 +11,7 @@ export const subcommand_help = factory.subCommand(makeSubCommand('help', 'Get He
 export const component_get_help_update = factory.component(
   makeButton('get_help_update', 'Update Help').style(buttonStyle.Secondary),
   (c): Response =>
-    c.res({
+    c.update().res({
       content: `Help update clicked +${c.ref.custom_value ?? 0}`,
       components: [
         makeActionRow([component_get_help_update.component.custom_value(String(Number(c.ref.custom_value ?? 0) + 1))]),
